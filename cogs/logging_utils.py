e53a1c9dc143905d7b606e34ecd737af2e3037dd
artem30801 <38689676+artem30801@users.noreply.github.com>
2021-04-07 20:36:24 +0300

    Added keepalive to logger sockets

--- cogs/logging_utils.py
+++ cogs/logging_utils.py
@@ -2,6 +2,49 @@ import logging.handlers
 import os
 import os.path
 import pickle
+import platform
+import socket
+
+def set_keepalive(sock, after_idle_sec=1, interval_sec=3, max_fails=5):
+    """
+    Sets `keepalive` parameters of given socket.
+
+    Args:
+        sock (socket): Socket which parameters will be changed.
+        after_idle_sec (int, optional): Start sending keepalive packets after this amount of seconds. Defaults to 1.
+        interval_sec (int, optional): Interval of keepalive packets in seconds. Defaults to 3.
+        max_fails (int, optional): Count of fails leading to socket disconnect. Defaults to 5.
+
+    Raises:
+        NotImplementedError: for unknown platform.
+    """
+    current_platform = platform.system()  # could be empty
+
+    if current_platform == "Linux":
+        return _set_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails)
+    if current_platform == "Windows":
+        return _set_keepalive_windows(sock, after_idle_sec, interval_sec)
+    if current_platform == "Darwin":
+        return _set_keepalive_osx(sock, interval_sec)
+
+    raise NotImplementedError
+
+
+def _set_keepalive_linux(sock, after_idle_sec, interval_sec, max_fails):
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
+def _set_keepalive_windows(sock, after_idle_sec, interval_sec):
+    sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, after_idle_sec * 1000, interval_sec * 1000))
+
+
+def _set_keepalive_osx(sock, interval_sec):
+    TCP_KEEPALIVE = 0x10
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, interval_sec)
 
 
 class BufferingSocketHandler(logging.handlers.SocketHandler):
@@ -21,6 +64,11 @@ class BufferingSocketHandler(logging.handlers.SocketHandler):
         except Exception:
             self.handleError(record)
             return False
+
+    def makeSocket(self, timeout=1):
+        result = super().makeSocket(timeout)
+        set_keepalive(result)
+        return result
 
     def emit(self, record):
         self.send_buffer()
