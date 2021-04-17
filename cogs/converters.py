5cad98a829f731ffba5eac120fbb54bc94391c87
Desman735 <desman735.dev@gmail.com>
2021-04-17 11:05:46 +0300

    Add temporary workaround for daylight saving time for PST and CST

--- cogs/converters.py
+++ cogs/converters.py
@@ -350,6 +350,11 @@ class Conversions(utils.AutoLogCog):
                 return None, None, TimezoneInputType.InvalidData
 
             result_type = TimezoneInputType.ValidTimezone
+        
+        if timezone == "PST":
+            timezone = "PDT"
+        if timezone == "CST":
+            timezone = "CDT"
 
         diff = await self.timezones_diff("UTC", timezone)
         result_type = result_type if diff is not None else TimezoneInputType.InvalidData
