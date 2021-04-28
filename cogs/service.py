827014194e07a23aa278963f26655e2891c7e6f4
artem30801 <38689676+artem30801@users.noreply.github.com>
2021-04-28 22:14:51 +0300

    Added logs command, added service commands description

--- cogs/service.py
+++ cogs/service.py
@@ -6,9 +6,10 @@ from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option, create_choice
 
 import cogs.cog_utils as utils
+import cogs.db_utils as db_utils
+
 from cogs.cog_utils import guild_ids
 from cogs.permissions import has_server_perms, has_bot_perms
-
 
 logger = logging.getLogger(__name__)
 
@@ -48,6 +49,7 @@ class Service(utils.AutoLogCog, utils.StartupCog):
     @cog_ext.cog_subcommand(base="bot", name="update", guild_ids=guild_ids)
     @has_bot_perms()
     async def update(self, ctx: SlashContext):
+        """Updates the bot with the latest version from GIT and restarts it"""
         await ctx.defer()
         output, error = await utils.run(f"(cd {self.bot.current_dir}; git pull)")
 
@@ -65,6 +67,7 @@ class Service(utils.AutoLogCog, utils.StartupCog):
     @cog_ext.cog_subcommand(base="bot", name="restart", guild_ids=guild_ids)
     @has_bot_perms()
     async def restart(self, ctx: SlashContext):
+        """Restarts the bot"""
         await ctx.send(":warning: **Restarting the bot!** :warning:")
         await self.bot.close()
         await utils.run(f"systemctl --user restart skycomicbot.service")
@@ -72,10 +75,34 @@ class Service(utils.AutoLogCog, utils.StartupCog):
     @cog_ext.cog_subcommand(base="bot", name="shutdown", guild_ids=guild_ids)
     @has_bot_perms()
     async def shutdown(self, ctx: SlashContext):
+        """Shutdowns the bot. Use in emergency only!"""
         await ctx.send(":warning: **Shutting down the bot!** :warning:")
         await self.bot.close()
         # It shouldn't be running after "close" but juuust in case add this
         await utils.run("systemctl --user stop skycomicbot.service")
+
+    @cog_ext.cog_subcommand(base="bot", name="logs",
+                            options=[create_option(
+                                name="lines",
+                                description="Amount of lines to show ()",
+                                option_type=int,
+                                required=False,
+                            )],
+                            guild_ids=guild_ids)
+    @has_bot_perms()
+    async def logs(self, ctx: SlashContext, lines=20):
+        """Shows specified amount of last lines from bots log"""
+        if lines > 200 or lines < 10:
+            raise commands.BadArgument("Lines amount should be between 10 and 200!")
+
+        logs, err = await utils.run(f"journalctl --user-unit skycomicbot.service -n {lines} -q --no-pager -o cat")
+        if err:
+            await ctx.send(f"Cannot fetch logs! Error {err}", hidden=True)
+            return
+
+        logs = logs.split("\n")
+        for chunk in db_utils.chunks_split(logs):
+            await ctx.send("\n".join(chunk))
 
 
 def setup(bot):
