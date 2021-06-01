6154bec4470f3a2c850aedde1e7b1b895cf55bb4
artem30801 <38689676+artem30801@users.noreply.github.com>
2021-06-01 23:44:28 +0300

    Refactored RSP game AGAIN, added tic tac toe game x2

--- cogs/games.py
+++ cogs/games.py
@@ -28,6 +28,17 @@ class GameStates(enum.Enum):
     has_winner = 3
 
 
+class PlayerStates(enum.Enum):
+    no_player = -1
+    waiting_move = 0
+    skipped_move = 1
+    made_move = 2
+
+
+class NoFreePlayerSlots(discord.DiscordException):
+    pass
+
+
 class UnoCard:
     special_types = ["+2", "reverse", "block", "wild", "+4"]
     colors = [ButtonStyle.red, ButtonStyle.blue, ButtonStyle.green, ButtonStyle.gray]
@@ -88,30 +99,64 @@ class UnoGame:
         pass
 
 
-class RSPGame:
-    def __init__(self, ctx, cog, players=None):
+class Game:
+    command_name: str
+    game_title: str
+
+    def __init__(self, ctx, cog):
         self.ctx = ctx
         self.cog = cog
 
         self.state = GameStates.waiting_move
-
         self.started_at = datetime.utcnow()
-        self.game_ended = False
 
-        self.players = players or [None, None]
-        self.moves = [None, None]
+        self.max_players = 0 or self.max_players
+        self.players = [None] * self.max_players
+        self.player_states = [PlayerStates.no_player] * self.max_players
 
-        self.buttons = [create_actionrow(
-            create_button(style=ButtonStyle.gray, label="Rock", emoji="🪨"),
-            create_button(style=ButtonStyle.gray, label="Paper", emoji="🧻"),
-            create_button(style=ButtonStyle.gray, label="Scissors", emoji="✂️"),
-        )]
+    def get_player_index(self, member: discord.Member):
+        try:
+            player_index = self.players.index(member)
+        except ValueError:  # sender is not in players list
 
-        self.moves_binding = {button["custom_id"]: button["label"].lower()
-                              for button in self.buttons[0]["components"]}
+            try:
+                player_index = self.players.index(None)
+            except ValueError:
+                raise NoFreePlayerSlots
+            else:
+                self.players[player_index] = member  # new player joined
+
+        return player_index
+
+    async def check_player_index(self, ctx):
+        try:
+            player_index = self.get_player_index(ctx.author)
+        except NoFreePlayerSlots:
+            await ctx.send(f"Sorry, but all player spots are already taken!\n"
+                           f"You can start a new game with `/play {self.command_name}` command!",
+                           hidden=True)
+            return
+
+        if self.player_states[player_index] is not PlayerStates.waiting_move:
+            await ctx.send("You already made your move! Wait for your opponent.", hidden=True)
+            return
+
+        return player_index
+
+    def get_next_player(self, player_index):
+        raise NotImplemented
+
+
+class TwoPlayerGame(Game):
+    def __init__(self, ctx, cog, players=None):
+        self.max_players = 2
+        super().__init__(ctx, cog)
+        self.players = players or self.players
+
+        self.buttons = []
 
     async def play(self):
-        embed = self.make_rsp_embed()
+        embed = self.make_embed()
         message = await self.ctx.send(embed=embed, components=self.buttons)
 
         to_edit = {}
@@ -119,10 +164,11 @@ class RSPGame:
             await asyncio.wait_for(self.wait_moves(message), timeout=self.cog.global_timeout)
         except (asyncio.CancelledError, asyncio.TimeoutError):
             self.state = GameStates.game_timeout
-            to_edit["embed"] = self.make_rsp_embed()
+            to_edit["embed"] = self.make_embed()
 
-        for button in self.buttons[0]["components"]:
-            button["disabled"] = True
+        for row in self.buttons:
+            for button in row["components"]:
+                button["disabled"] = True
         to_edit["components"] = self.buttons
 
         await message.edit(**to_edit)
@@ -133,61 +179,23 @@ class RSPGame:
             await self.player_move(button_ctx)
 
     async def player_move(self, button_ctx: ComponentContext):
-        try:
-            player_index = self.players.index(button_ctx.author)
-        except ValueError:  # sender is not in players list
-            try:
-                player_index = self.players.index(None)
-            except ValueError:  # no free places
-                await button_ctx.send("Sorry, but all player spots are already taken!\n"
-                                      "You can start a new game with `/game rock_paper_scissors` command!",
-                                      hidden=True)
-                return
-            else:
-                self.players[player_index] = button_ctx.author  # new player joined
+        raise NotImplementedError
 
-        if self.moves[player_index]:
-            await button_ctx.send("You already made your move! Wait for your opponent.", hidden=True)
-            return
-
-        move_str = self.moves_binding[button_ctx.custom_id]
-        self.moves[player_index] = move_str
-        logger.debug(f"Player {player_index} ({button_ctx.author}) made a move: {move_str} ")
-
-        if all(self.moves):
-            self.state = GameStates.has_winner
-
-        embed = self.make_rsp_embed()
-        await button_ctx.edit_origin(embed=embed)
-
-    def get_winner(self):
-        moves = sorted(self.moves)
-        if moves[0] == moves[1]:
-            return None, "Nobody lose, nobody wins! Rematch?"
-        if moves[0] == "paper":
-            if moves[1] == "rock":
-                return "paper", "{} totally wiped out {}!"
-            if moves[1] == "scissors":
-                return "scissors", "{} cut {} in half!"
-        else:
-            return "rock", "{} rocks! And {} is crushed!"
-
-    def make_rsp_embed(self):
+    def make_embed(self):
         embed = utils.bot_embed(self.cog.bot)
-        embed.title = "Rock 🪨! Paper 🧻! Scissors ✂️!"
+        embed.title = self.game_title
 
         winner_index = None
         if self.state is GameStates.has_winner:
-            winner, win_text = self.get_winner()
-            if winner:
-                winner_index = self.moves.index(winner)
+            winner_index, win_text = self.get_winner()
+            if winner_index is not None:
                 embed.add_field(name=f"🎉 {self.players[winner_index].name} won! 🎉",
                                 value=win_text.format(self.players[winner_index].display_name,
-                                                      self.players[winner_index - 1].display_name),
+                                                      self.players[self.get_next_player(winner_index)].display_name),
                                 inline=False)
             else:
                 embed.add_field(name="Draw!", value=win_text, inline=False)
-            embed.set_footer(text="Use `/play rock_paper_scissors` for another game!",
+            embed.set_footer(text=f"Use `/play {self.command_name}` for another game!",
                              icon_url=self.cog.bot.user.avatar_url)
         elif self.state is GameStates.game_timeout:
             embed.set_footer(text="Game ended at", icon_url=self.cog.bot.user.avatar_url)
@@ -200,24 +208,196 @@ class RSPGame:
         for i, player in enumerate(self.players):
             if player:
                 text = f"{player.mention}\n{player}"
-                if move := self.moves[i]:
-                    if self.state is GameStates.waiting_move:
-                        text += "\n Already made a move!"
-                    else:
-                        text += f"\n Made a move: {rsp_emojis[move]}"
-                else:
-                    text += f"\n Haven't made a move yet!"
+                text += self.additional_player_text(i)
             else:
                 text = "Nobody wanted to play :(" if self.state is GameStates.game_timeout \
                     else "Free spot!\nMake a move to join the game!"
 
-            title = f"Player {i + 1}"
+            title = self.get_player_title(i)
             if winner_index == i:
                 title += "- winner!"
 
             embed.insert_field_at(i, name=title, value=text)
 
         return embed
+
+    def additional_player_text(self, player_index):
+        return ""
+
+    def get_player_title(self, player_index):
+        return f"Player {player_index + 1}"
+
+    def get_winner(self):
+        raise NotImplementedError
+
+    def get_next_player(self, player_index):
+        return 0 if player_index == 1 else 1
+
+
+class RPSGame(TwoPlayerGame):
+    command_name = "rock_paper_scissors"
+    game_title = "Rock 🪨! Paper 🧻! Scissors ✂️!"
+
+    def __init__(self, ctx, cog, players=None):
+        super().__init__(ctx, cog, players)
+
+        self.moves = [None, None]
+        self.player_states = [PlayerStates.waiting_move] * self.max_players
+
+        self.buttons = [create_actionrow(
+            create_button(style=ButtonStyle.gray, label="Rock", emoji="🪨"),
+            create_button(style=ButtonStyle.gray, label="Paper", emoji="🧻"),
+            create_button(style=ButtonStyle.gray, label="Scissors", emoji="✂️"),
+        )]
+
+        self.moves_binding = {button["custom_id"]: button["label"].lower()
+                              for button in self.buttons[0]["components"]}
+
+    async def player_move(self, button_ctx: ComponentContext):
+        if (player_index := await self.check_player_index(button_ctx)) is None:
+            return
+
+        move_str = self.moves_binding[button_ctx.custom_id]
+        self.moves[player_index] = move_str
+        self.player_states[player_index] = PlayerStates.made_move
+        logger.debug(f"Player {player_index} ({button_ctx.author}) made a move: {move_str} ")
+
+        if all(self.moves):
+            self.state = GameStates.has_winner
+
+        embed = self.make_embed()
+        await button_ctx.edit_origin(embed=embed)
+
+    def get_winner(self):
+        winner, message = self._get_winner()
+        winner_index = self.moves.index(winner)
+        return winner_index, message
+
+    def _get_winner(self):
+        moves = sorted(self.moves)
+        if moves[0] == moves[1]:
+            return None, "Nobody lose, nobody wins! Rematch?"
+        if moves[0] == "paper":
+            if moves[1] == "rock":
+                return "paper", "{} totally wiped out {}!"
+            if moves[1] == "scissors":
+                return "scissors", "{} cut {} in half!"
+        else:
+            return "rock", "{} rocks! And {} is crushed!"
+
+    def additional_player_text(self, player_index):
+        if self.state is GameStates.has_winner:
+            return f"\n Made a move: {rsp_emojis[self.moves[player_index]]}"
+        elif self.player_states[player_index] is PlayerStates.made_move:
+            return "\n Already made a move!"
+        elif self.player_states[player_index] is PlayerStates.waiting_move:
+            return f"\n Haven't made a move yet!"
+
+
+class TTTGame(TwoPlayerGame):
+    command_name = "tic_tac_toe"
+    game_title = "🟢 Tic tac toe! ❌"
+
+    empty_tile = " "
+
+    def __init__(self, ctx, cog, players=None, size=3):
+        super().__init__(ctx, cog, players)
+        self.player_states[0] = PlayerStates.waiting_move
+        self.size = size
+
+        self.winner_index = None
+        self.move_count = 0
+
+        self.buttons = []
+        self.moves_binding = {}
+        for i in range(size):
+            row = []
+            for j in range(size):
+                button = create_button(style=ButtonStyle.gray, label=self.empty_tile)
+                row.append(button)
+                self.moves_binding[button["custom_id"]] = (i, j)
+            self.buttons.append(create_actionrow(*row))
+
+    def player_place(self, player_index):
+        placement = [("O", ButtonStyle.green), ("X", ButtonStyle.red)]
+        return placement[player_index]
+
+    def get_button(self, i, j):
+        return self.buttons[i]["components"][j]
+
+    async def player_move(self, button_ctx: ComponentContext):
+        if (player_index := await self.check_player_index(button_ctx)) is None:
+            return
+
+        i, j = self.moves_binding[button_ctx.custom_id]
+        button = self.get_button(i, j)
+        if button["label"] != self.empty_tile:
+            await button_ctx.send("Sorry, but this tile is already taken! Make another move!", hidden=True)
+            return
+        move_str, color = self.player_place(player_index)
+        button["label"] = move_str
+        button["style"] = color
+
+        self.player_states[player_index] = PlayerStates.made_move
+        self.player_states[self.get_next_player(player_index)] = PlayerStates.waiting_move
+
+        self.move_count += 1
+
+        logger.debug(f"Player {player_index} ({button_ctx.author}) made a move: {move_str} ({i}, {j}) ")
+
+        if self.check_winner(move_str, i, j):
+            self.state = GameStates.has_winner
+            self.winner_index = player_index
+        elif self.move_count == self.size ** 2:
+            self.state = GameStates.has_winner
+
+        embed = self.make_embed()
+        await button_ctx.edit_origin(embed=embed, components=self.buttons)
+
+    def get_winner(self):
+        text = "Ney, it's a tie! Wanna try again?" if self.winner_index is None else "{} won against {}!"
+        return self.winner_index, text
+
+    def check_winner(self, move_str, i, j):
+        horizontal = self.check_line(move_str, i, j, 0, 1)
+        vertical = self.check_line(move_str, i, j, 1, 0)
+        diagonal1 = self.check_line(move_str, i, j, 1, 1)
+        diagonal2 = self.check_line(move_str, i, j, 1, -1)
+        return horizontal or vertical or diagonal1 or diagonal2
+
+    def check_line(self, move_str, i, j, dx=0, dy=0):
+        count = self.check_line_side(move_str, i, j, dx, dy) + \
+                self.check_line_side(move_str, i, j, dx * -1, dy * -1) - 1
+        return count == self.size
+
+    def check_line_side(self, move_str, i, j, dx=0, dy=0):
+        count = 0
+        while 0 <= i < self.size and 0 <= j < self.size:
+            if self.get_button(i, j)["label"] == move_str:
+                count += 1
+            else:
+                return count
+
+            i += dx
+            j += dy
+
+        return count
+
+    def additional_player_text(self, player_index):
+        if self.state is GameStates.has_winner:
+            if player_index == self.winner_index:
+                return "\n Made winning move!"
+            else:
+                return "\n Didn't win this time"
+
+        elif self.player_states[player_index] is PlayerStates.made_move:
+            return "\n Already made a move!"
+        elif self.player_states[player_index] is PlayerStates.waiting_move:
+            return f"\n Make your move now!"
+
+    def get_player_title(self, player_index):
+        symbols = ["🟢", "❌"]  # ⭕
+        return f"{symbols[player_index]} " + super().get_player_title(player_index)
 
 
 class Games(utils.AutoLogCog, utils.StartupCog):
@@ -230,6 +410,16 @@ class Games(utils.AutoLogCog, utils.StartupCog):
         self.bot = bot
         self.global_timeout = 15 * 60
         self.move_timeout = 1 * 60
+
+    async def check_2_players(self, ctx, player1, player2):
+        players = [player1, player2]
+
+        if player1 == player2 and player1 and player2:
+            raise commands.BadArgument("You can't set one member as two players at once!")
+        if (player1 and player1.bot) or (player2 and player2.bot):
+            raise commands.BadArgument("We bots are smart but we cant play games yet ;)")
+
+        return players
 
     @cog_ext.cog_subcommand(base="play", name="rock_paper_scissors",
                             options=[
@@ -249,17 +439,23 @@ class Games(utils.AutoLogCog, utils.StartupCog):
                             ],
                             guild_ids=guild_ids)
     async def rock_paper_scissors(self, ctx: SlashContext, player1=None, player2=None):
-        players = [player1, player2]
-        if player1 == player2 and player1 and player2:
-            raise commands.BadArgument("You can't set one member as two players at once!")
-        if (player1 and player1.bot) or (player2 and player2.bot):
-            raise commands.BadArgument("We bots are smart but we cant play rock paper scissors yet ;)")
-
-        game = RSPGame(ctx, self, players)
+        """Multiplayer game! Assert dominance in epic battle of luck and wits!"""
+        players = await self.check_2_players(ctx, player1, player2)
+        game = RPSGame(ctx, self, players)
         await game.play()
 
     @cog_ext.cog_subcommand(base="play", name="tic_tac_toe",
                             options=[
+                                create_option(
+                                    name="size",
+                                    description="Size of the field",
+                                    option_type=int,
+                                    required=False,
+                                    choices=[create_choice(name="3/3", value=3),
+                                             create_choice(name="4/4", value=4),
+                                             create_choice(name="5/5", value=5),
+                                             ]
+                                ),
                                 create_option(
                                     name="player1",
                                     description="Specify first player",
@@ -272,40 +468,13 @@ class Games(utils.AutoLogCog, utils.StartupCog):
                                     option_type=discord.Member,
                                     required=False,
                                 ),
-                                create_option(
-                                    name="size",
-                                    description="Size of the field",
-                                    option_type=int,
-                                    required=False,
-                                    choices=[create_choice(name="3/3", value=3),
-                                             create_choice(name="4/4", value=4),
-                                             create_choice(name="5/5", value=5),
-                                             ]
-                                ),
-
                             ],
                             guild_ids=guild_ids)
     async def tic_tac_toe(self, ctx, player1=None, player2=None, size=3):
-        players = [player1, player2]
-        if player1 == player2 and player1 and player2:
-            raise commands.BadArgument("You can't set one member as two players at once!")
-        if (player1 and player1.bot) or (player2 and player2.bot):
-            raise commands.BadArgument("We bots are smart but we cant play rock paper scissors yet ;)")
-
-        # components = [create_actionrow(
-        #     create_button(style=ButtonStyle.gray, label="Rock", emoji="🪨"),
-        #     create_button(style=ButtonStyle.gray, label="Paper", emoji="🧻"),
-        #     create_button(style=ButtonStyle.gray, label="Scissors", emoji="✂️"),
-        # )]
-        components = []
-        components_moves = {}
-        for i in range(size):
-            row = []
-            for j in range(size):
-                button = create_button(style=ButtonStyle.gray, label="")
-                row.append(button)
-                components_moves[button["custom_id"]] = (i, j)
-            components.append(create_actionrow(*row))
+        """Multiplayer game! Tic-tac-toe! Noughts and crosses! Xs and Os! Play with friends!"""
+        players = await self.check_2_players(ctx, player1, player2)
+        game = TTTGame(ctx, self, players, size)
+        await game.play()
 
 
 def setup(bot):
